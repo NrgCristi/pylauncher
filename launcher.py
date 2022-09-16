6a0466bb3fbb89c204cee6d53a930261532f90fd
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-16 16:51:34 +0300

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,6 +1,6 @@
 import os
 os.system("pip install os")
 input('please enter your fortnite path:')
-os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true')
 input('please enter the launcher arguments that you want to use:')
+os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true')
 print('Launching Fortnite')
