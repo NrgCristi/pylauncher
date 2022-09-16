117e35ece5c0e41dd96fb931956fdbb984843ee2
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-16 19:09:29 +0300

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,6 +1,7 @@
 import os
 os.system("pip install os")
-input('please enter your fortnite path:')
 input('please enter the launcher arguments that you want to use:')
-os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true')
 print('Launching Fortnite')
+os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true')
+print('Closing In 1 Minute')
+time.sleep(60)
