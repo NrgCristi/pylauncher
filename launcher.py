349ed515e866682c94377433f00e546d3e6095e9
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-18 12:33:34 +0300

    Delete launcher.py

--- launcher.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("pip install os")
-input('please enter the launcher arguments that you want to use:')
-print('Launching Fortnite')
-os.startfile('com.epicgames.launcher://apps/fn%3A4fe75bbc5a674f4f9b356b5c90567da5%3AFortnite?action=launch&silent=true')
-print('Closing In 1 Minute')
-time.sleep(60)
