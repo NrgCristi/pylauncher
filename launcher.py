7bdd51db0a3b077d7be4c967965baafe57fe5fac
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-18 12:34:00 +0300

    Add files via upload

---
+++ launcher.py
@@ -0,0 +1,10 @@
+import os
+os.system("pip install os")
+input('Please Put Your Fortnite Path:')
+input('please enter the launcher arguments that you want to use:')
+print('Launching Fortnite')
+path = "/FortniteGame/Binaries/Win64/FortniteClient-Win64-Shipping.exe"
+print(os.path.join(path, "/FortniteGame/Binaries/Win64/", "FortniteClient-Win64-Shipping.exe", "/home"))
+os.startfile("path")
+print('Closing In 1 Minute')
+time.sleep(60)
